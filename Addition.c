864d6359c83037c61ce4ed4b96e61502f6aca32d
Vikas Sharma <vikkysharma0202@gmail.com>
2019-01-30 23:39:27 +0530

    Update REanme

--- Addition.c
+++ Addition.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
 printf("I love my Country \n");
-printf("I love my Changed Country and College\n");   
+printf("I love my Changed Country and College Both\n");   
 return 0;
 }
